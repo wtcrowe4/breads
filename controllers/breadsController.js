70ff63ab4a1618967b56a712610aec631c8b07b3
Thomas Crowe <wcrowe@g.clemson.edu>
2022-07-23 15:46:41 -0400

    Refactored index, show, create to use database

--- controllers/breadsController.js
+++ controllers/breadsController.js
@@ -4,25 +4,29 @@ const BREAD = require('../models/bread.js')
 
 //Index
 breads.get('/', (req, res) => {
-    res.render('index',
-        {
-            breads: BREAD, 
-            title: 'Index Page'
-        }
-    )
+    BREAD.find()
+        .then(foundBreads => {
+            res.render('index',
+                {
+                    breads: foundBreads, 
+                    title: 'Index Page'
+                }
+            )
+        })
 }) 
 
 //Create
 breads.post('/', (req, res) => {
+    console.log(req.body)
     if (!req.body.image) {
-        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+        req.body.image = undefined
     }
     if(req.body.hasGluten === 'on') {
         req.body.hasGluten = 'true'
     } else {
         req.body.hasGluten = 'false'
     }
-    BREAD.push(req.body)
+    BREAD.create(req.body)
     res.redirect('/breads')
 })
 
@@ -40,16 +44,20 @@ breads.get('/:arrayIndex/edit', (req, res) => {
 })
 
 //Show
-breads.get('/:arrayIndex', (req, res) => {
-    if (BREAD[req.params.arrayIndex]) {
-        res.render('Show', {
-            bread: BREAD[req.params.arrayIndex],
-            index: [req.params.arrayIndex]
-        })
-    } else if(!BREAD[req.params.arrayIndex]) {
-        res.render('404')
-    }
+breads.get('/:id', (req, res) => {
+    BREAD.findById(req.params.id)
+        .then(foundBread => res.render('Show', { bread: foundBread }))
+        .catch(error => res.render('404'))
 })
+//     if (BREAD[req.params.arrayIndex]) {
+//         res.render('Show', {
+//             bread: BREAD[req.params.arrayIndex],
+//             index: [req.params.arrayIndex]
+//         })
+//     } else if(!BREAD[req.params.arrayIndex]) {
+//         res.render('404')
+//     }
+// })
 
 //Delete
 breads.delete('/:arrayIndex', (req, res) => {
--- views/show.jsx
+++ views/show.jsx
@@ -2,26 +2,24 @@ const React = require('react')
 const Def = require('./layouts/default')
 
 const Show = ({bread, index}) => {
-  //console.log(bread.name)
-    return (
-      <Def>
-        <h2>Show Page</h2>
-        <h3>{bread.name}</h3>
-        <p>
-            {bread.name} bread {
-                bread.hasGluten
-                ? <span> does </span>
-                : <span> does not </span>
-            }
-            have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-        <form action={`/breads/${index}?_method=DELETE`} method="POST">
-          <input type='submit' value='Delete' />
-        </form>
-      </Def>
-    )
+  return (
+    <Def>
+      <h2>Show Page</h2>
+      <h3>{bread.name}</h3>
+      <p>
+        {bread.name} bread
+          { bread.hasGluten
+          ? <span> does </span>
+          : <span> does not </span> }
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value='Delete' />
+      </form>
+    </Def>
+  )
 }
 
 module.exports = Show
